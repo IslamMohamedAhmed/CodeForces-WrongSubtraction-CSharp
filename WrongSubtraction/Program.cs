6485c2bc61c7c0e2a874a95f2fd056a6938d91ea
unknown <islammohamedahmed49@gmail.com>
2024-04-13 12:48:34 +0200

    ws

---
+++ WrongSubtraction/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+using static System.Convert;
+using static System.Math;
+
+
+long n; int k;
+
+string str = ReadLine();
+var arr = str.Split(" ");
+n = ToInt64(arr[0]);
+k = ToInt32(arr[1]);
+
+for(int i = 0; i < k; i++)
+{
+    if(n%10 == 0)
+    {
+        n /= 10;
+    }
+    else
+    {
+        n--;
+    }
+}
+
+WriteLine(n);
